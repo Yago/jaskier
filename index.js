5b135e99c099b47ccc8864e9f863055e21d2355f
Yann Gouffon <github@yago-art.ch>
2017-05-22 20:33:08 +0200

    ✨ add time log

--- index.js
+++ index.js
@@ -3,34 +3,52 @@ const chalk = require('chalk');
 const isBrowser = process.title === 'browser';
 const methods = [
   "warn"
-]
+];
 
 class Logger {
   constructor() {
-    // this.console = console;
+    this.defaultStyle = 'color: #A6B2C0;';
     this.console = {};
-
     methods.forEach((method) => {
       this.console[method] = console[method];
-    })
+    });
   }
 
   print(content, opts) {
     let input = content;
     if (typeof content === 'string') input = { content };
 
+    const date = new Date;
+    const currentTime = `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`;
+
     [...input].forEach((item) => {
       if (typeof item === 'string') {
         if (isBrowser) {
-          this.console[opts.method](`%c ${opts.emoji} ${item}`, opts.browser);
+          this.console[opts.method](
+            `%c${currentTime} %c${opts.emoji} ${item}`,
+            this.defaultStyle,
+            opts.browser
+          );
         } else {
-          this.console[opts.method](chalk[opts.server](`${opts.emoji}  ${item}`));
+          this.console[opts.method](
+            chalk.grey(currentTime),
+            chalk[opts.server](`${opts.emoji}  ${item}`)
+          );
         }
       } else {
         if (isBrowser) {
-          this.console[opts.method](`%c ${opts.emoji}`, opts.browser, item);
+          this.console[opts.method](
+            `%c${currentTime} %c${opts.emoji}`,
+            this.defaultStyle,
+            opts.browser,
+            item
+          );
         } else {
-          this.console[opts.method](chalk[opts.server](`${opts.emoji} `), item);
+          this.console[opts.method](
+            chalk.grey(currentTime),
+            chalk[opts.server](`${opts.emoji} `),
+            item
+          );
         }
       }
       
